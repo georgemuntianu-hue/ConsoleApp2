f154000fe1833c0e8edb71722cf29c04ec1562ed
georgemuntianu-hue <george.muntianu@student.usv.ro>
2026-03-09 21:47:01 +0200

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // 1. Instanțierea magazinului
+        // 2. Crearea și adăugarea obiectelor de tip PiesaAuto
+        // 3. Apelarea funcțiilor de căutare și filtrare
+        // 4. Afișarea rezultatelor în consolă
+    }
+}
