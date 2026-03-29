339c583e7ada9863cd532cb200458f70f2e539dd
georgemuntianu-hue <george.muntianu@student.usv.ro>
2026-03-29 14:05:41 +0300

    Refactor Program to use AdministrarePieseFisierText

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using GestiunePieseAuto;
+using StocareDate;
 
 namespace GestiunePieseAuto
 {
@@ -8,16 +9,19 @@ namespace GestiunePieseAuto
     {
         static void Main(string[] args)
         {
-            MagazinAuto magazin = new MagazinAuto();
+            AdministrarePieseFisierText admin =
+                new AdministrarePieseFisierText("piese.txt");
+
             bool continua = true;
 
             while (continua)
             {
                 Console.WriteLine("\n--- MENU GESTIUNE PIESE AUTO ---");
-                Console.WriteLine("1. Adauga Piesa (Citire date)");
+                Console.WriteLine("1. Adauga Piesa");
                 Console.WriteLine("2. Afiseaza Toate Piesele");
                 Console.WriteLine("3. Cauta dupa Nume/Cod");
                 Console.WriteLine("4. Filtrare dupa Pret Maxim");
+                Console.WriteLine("5. Modifica Pretul unei Piese");
                 Console.WriteLine("0. Iesire");
                 Console.Write("Alege optiunea: ");
 
@@ -26,33 +30,56 @@ namespace GestiunePieseAuto
                 switch (optiune)
                 {
                     case "1":
-                        Console.Write("Nume piesa: "); string nume = Console.ReadLine();
-                        Console.Write("Cod piesa: "); string cod = Console.ReadLine();
-                        Console.Write("Pret: "); double pret = double.Parse(Console.ReadLine());
-                        Console.Write("Locatie (Oras): "); string loc = Console.ReadLine();
+                        Console.Write("Nume piesa: ");
+                        string nume = Console.ReadLine();
+
+                        Console.Write("Cod piesa: ");
+                        string cod = Console.ReadLine();
+
+                        Console.Write("Pret: ");
+                        double pret = double.Parse(Console.ReadLine());
+
+                        Console.Write("Locatie (Oras): ");
+                        string loc = Console.ReadLine();
+
                         Console.Write("Este disponibila online? (da/nu): ");
                         bool online = Console.ReadLine().ToLower() == "da";
 
                         PiesaAuto pusaNoua = new PiesaAuto(nume, cod, pret, loc, online);
-                        magazin.AdaugaPiesa(pusaNoua);
+
+                        admin.AdaugaPiesa(pusaNoua);
 
                         Console.WriteLine("Piesa a fost salvata!");
                         break;
 
                     case "2":
-                        AfiseazaLista(magazin.GetToatePiesele());
+                        AfiseazaLista(admin.GetPiese());
                         break;
 
                     case "3":
                         Console.Write("Introdu numele sau codul: ");
                         string termen = Console.ReadLine();
-                        AfiseazaLista(magazin.CautaPiesa(termen));
+
+                        AfiseazaLista(admin.CautaPiesa(termen));
                         break;
 
                     case "4":
                         Console.Write("Introdu pretul maxim: ");
                         double pMax = double.Parse(Console.ReadLine());
-                        AfiseazaLista(magazin.FiltreazaDupaPret(pMax));
+
+                        AfiseazaLista(admin.FiltreazaDupaPret(pMax));
+                        break;
+
+                    case "5":
+                        Console.Write("Introdu codul piesei: ");
+                        string codMod = Console.ReadLine();
+
+                        Console.Write("Introdu pretul nou: ");
+                        double pretNou = double.Parse(Console.ReadLine());
+
+                        admin.ModificaPret(codMod, pretNou);
+
+                        Console.WriteLine("Pret modificat!");
                         break;
 
                     case "0":
